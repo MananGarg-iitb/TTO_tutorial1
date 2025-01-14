bf26c450863965ff5d62a490380210bce5c01484
MananGarg-iitb <144512698+MananGarg-iitb@users.noreply.github.com>
2025-01-14 23:02:33 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,13 +15,15 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+    
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
+    assign uo_out[5:0] = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire _unused = &{ena, uio_in, ui_in[4:0], clk, rst_n, 1'b0};
 
+
+    Full_Adder uut (.A(ui_in[7]), .B(ui_in[6]), .Cin(ui_in[5]), .S(uo_out[7]), .Cout(uo_out[6]));
 endmodule
