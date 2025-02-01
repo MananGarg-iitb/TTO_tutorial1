670c59d1ac2bcc8a87a8e67802d88f3f976e9557
MananGarg-iitb <144512698+MananGarg-iitb@users.noreply.github.com>
2025-02-01 14:18:29 +0530

    did a small fix

--- src/project.v
+++ src/project.v
@@ -21,5 +21,5 @@ module tt_um_example (
   // List all unused inputs to prevent warnings
     wire _unused = &{ena, ui_in[7:3], 1'b0};
 
-    Toplevel uut (.clk(clk),.Bus_Out(uio_out),.Bus_In(uio_in),.rst(~rst_n),.En(uo_out[0]), .Rw(uo_out[1]), .pause(ui_in[0]), .regSelect(ui_in[2:1]), .dispReg(uo_out[7:3]) ,.ALE(uo_out[2]), .TT_En(uio_oe))
+    Toplevel uut (.clk(clk),.Bus_Out(uio_out),.Bus_In(uio_in),.rst(~rst_n),.En(uo_out[0]), .Rw(uo_out[1]), .pause(ui_in[0]), .regSelect(ui_in[2:1]), .dispReg(uo_out[7:3]) ,.ALE(uo_out[2]), .TT_En(uio_oe));
 endmodule
